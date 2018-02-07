738fec9acfbf0aa3d15899ee28dc6b5c2f8e2361
Felipe Roveran <feliperoveran@hotmail.com>
2018-02-06 23:25:46 -0200

    refactor sieve so it does not calculate the modulo of each number

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -4,28 +4,19 @@ class Sieve
   end
 
   def primes
-    primes = (2..@max).to_a
+    primes = [nil, nil, *2..@max]
 
-    multiples = (2..Math.sqrt(@max)).to_a
+    multiples = 2..Math.sqrt(@max)
 
     multiples.each do |i|
-      # index from where we will start looking for primes. so when i = 2 we
-      # start looking from the next element, that is, 3
-      index = primes.index(i)
+      range = i**2..@max
 
-      # Skip if we couln't find i. It has already been deleted on a previous
-      # interaction since it is a multiple of a previous value of i
-      next unless index
-
-      start_at = index + 1
-
-      primes[start_at..-1].each do |n|
-        # delete n if it is a multiple of i
-        primes.delete(n) if n % i == 0
+      range.step(i).each do |r|
+        primes[r] = nil
       end
     end
 
-    primes
+    primes.compact
   end
 end
 
