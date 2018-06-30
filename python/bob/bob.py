53faaac6b0cf3f8af0dd891333b9f62537dc8c78
Felipe Roveran <feliperoveran@hotmail.com>
2018-06-30 17:53:49 -0300

    add python solution to bob problem

---
+++ python/bob/bob.py
@@ -0,0 +1,15 @@
+import re
+
+def hey(phrase):
+    phrase = phrase.rstrip()
+
+    if re.match('^\s*$', phrase):
+        return 'Fine. Be that way!'
+    if re.match('[A-Z\s]*\?\Z', phrase):
+        return "Calm down, I know what I'm doing!"
+    elif re.match('.*\?\Z', phrase):
+        return 'Sure.'
+    elif re.match('[A-Z\s]+\Z', phrase) or re.match('[A-Z \W\d]+!+\Z', phrase):
+        return 'Whoa, chill out!'
+    else:
+        return 'Whatever.'
